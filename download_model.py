abfc3e9aaa7db22810dca8ab71ae9f7bb72f8b96
fantasy <van.mai.ne1004@gmail.com>
2026-03-15 06:50:34 +1000

    download model

--- download_model.py
+++ download_model.py
@@ -1,13 +1,8 @@
-# download_model.py
-import os
-from dotenv import load_dotenv
 from huggingface_hub import snapshot_download
-load_dotenv()
 
 snapshot_download(
-    repo_id="black-forest-labs/FLUX.1-dev",
+    repo_id="blackhole0110/FLUX.1-dev/tree/main",
     local_dir="/app/flux1-dev.safetensors",
-    token=os.environ["HF_TOKEN"],
     ignore_patterns=["*.md", "*.txt"],
 )
 
