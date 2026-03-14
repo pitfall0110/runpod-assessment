1ee0fa56e3037cdc4e2fdf651a4866dd64d825a8
fantasy <van.mai.ne1004@gmail.com>
2026-03-15 07:05:31 +1000

    repo name updae

--- download_model.py
+++ download_model.py
@@ -1,7 +1,7 @@
 from huggingface_hub import snapshot_download
 
 snapshot_download(
-    repo_id="blackhole0110/FLUX.1-dev/tree/main",
+    repo_id="blackhole0110/FLUX.1-dev",
     local_dir="/app/flux1-dev.safetensors",
     ignore_patterns=["*.md", "*.txt"],
 )
